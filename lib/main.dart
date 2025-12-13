0c6fa19d94aa46fea8edaf2957c80e218c296275
JoshvinSu <joshvin.1222@gmail.com>
2025-12-13 12:29:44 +0700

    menambahkan fitur akses kamera

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'screen/signin.dart';
-import './database/evergreen_db.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:awesome_notifications/awesome_notifications.dart';
